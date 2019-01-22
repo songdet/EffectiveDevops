ccb4a8b5363740de8bc1ed138986caa9a2e728b7
Songdet Nillasithanukroh <songdet@gmail.com>
2019-01-21 22:38:17 -0500

    integrate with terraform

---
+++ helloworld/terraform/ec2.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "myserver" {
+  ami = "ami-cfe4b2b0"
+  instance_type = "t2.micro"
+  key_name = "HomeLaptop"
+  vpc_security_group_ids = ["sg-023e46cd6251b6519"]
+
+  tags {
+    Name = "helloworld"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      user = "ec2-user"
+      private_key = "${file("~/.ssh/id_rsa")}"
+    }
+    inline = [
+      "sudo yum install --enablerepo=epel -y ansible git",
+      "sudo ansible-pull -U https://github.com/songdet/EffectiveDevops helloworld/ansible/helloworld.yml -i localhost",
+    ]
+  }
+}
+
+output "myserver" {
+ value = "${aws_instance.myserver.public_ip}"
+}
